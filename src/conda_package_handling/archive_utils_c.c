47ebb43c7c25edddc37e72e7b89f35ee27a34fc6
Alan Du <alanhdu@gmail.com>
2019-11-06 23:49:27 -0500

    Silence compiler warning

--- src/conda_package_handling/archive_utils_c.c
+++ src/conda_package_handling/archive_utils_c.c
@@ -142,7 +142,7 @@ static int extract_file_c(const char *filename, const char **err_str) {
     int r;
 
     if (!err_str) {
-        return NULL;
+        return 0;
     }
     /* attributes we want to restore. */
     flags = ARCHIVE_EXTRACT_TIME;
