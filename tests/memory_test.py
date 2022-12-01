9e5b70d2a0b2f52e18afe40db0c74c31d1926314
Daniel Holth <dholth@anaconda.com>
2022-11-30 21:42:49 -0500

    guard script

--- tests/memory_test.py
+++ tests/memory_test.py
@@ -6,12 +6,13 @@ import sys
 
 import zstandard
 
-times = 100
-if len(sys.argv) == 3:
-    times = int(sys.argv[2])
+if __name__ == "__main__":
+    times = 100
+    if len(sys.argv) == 3:
+        times = int(sys.argv[2])
 
-for i in range(times):
-    compressor = zstandard.ZstdCompressor(level=int(sys.argv[1]))
-    writer = compressor.stream_writer(io.BytesIO())
-    writer.write(b"hello")
-    writer.close()
+    for i in range(times):
+        compressor = zstandard.ZstdCompressor(level=int(sys.argv[1]))
+        writer = compressor.stream_writer(io.BytesIO())
+        writer.write(b"hello")
+        writer.close()
