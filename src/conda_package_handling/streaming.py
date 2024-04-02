cdc93c0d8e2c0583d05bfd9578be581a8b2836a9
jaimergp <jaimergp@users.noreply.github.com>
2024-04-02 18:44:11 +0200

    more exception work

--- src/conda_package_handling/streaming.py
+++ src/conda_package_handling/streaming.py
@@ -17,7 +17,7 @@ from . import exceptions
 
 
 def _stream_components(
-    filename: str, components: list[str]
+    filename: str, components: list[str], dest_dir: str = "",
 ) -> Generator[tuple[TarFile, TarInfo], None, None]:
     if str(filename).endswith(".tar.bz2"):
         assert components == ["pkg"]
@@ -29,6 +29,8 @@ def _stream_components(
                 yield package_streaming.stream_conda_component(
                     filename, fileobj, component=component
                 )
+    except cps_exceptions.CaseInsensitiveFileSystemError as e:
+        raise exceptions.CaseInsensitiveFileSystemError(filename, dest_dir) from e
     except (OSError, TarError, BadZipFile) as e:
         raise exceptions.InvalidArchiveError(filename, f"failed with error: {str(e)}") from e
 
@@ -44,13 +46,13 @@ def _extract(filename: str, dest_dir: str, components: list[str]):
     Internal. Skip directly to conda-package-streaming if you don't need
     exception compatibility.
     """
-    try:
-        for stream in _stream_components(filename, components):
+    for stream in _stream_components(filename, components, dest_dir=dest_dir):
+        try:
             extract_stream(stream, dest_dir)
-    except cps_exceptions.CaseInsensitiveFileSystemError as e:
-        raise exceptions.CaseInsensitiveFileSystemError(filename, dest_dir) from e
-    except (OSError, TarError, BadZipFile) as e:
-        raise exceptions.InvalidArchiveError(filename, f"failed with error: {str(e)}") from e
+        except cps_exceptions.CaseInsensitiveFileSystemError as e:
+            raise exceptions.CaseInsensitiveFileSystemError(filename, dest_dir) from e
+        except (OSError, TarError, BadZipFile) as e:
+            raise exceptions.InvalidArchiveError(filename, f"failed with error: {str(e)}") from e
 
 
 def _list(filename: str, components: list[str], verbose=True):
