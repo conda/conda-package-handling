448a888c8e8e4548d7e66f75eb989a330e633637
Daniel Holth <dholth@anaconda.com>
2024-10-24 14:44:45 +0200

    add assert

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,5 +9,5 @@ def test_checksum(tmp_path):
     with tmp_file.open("rb") as f:
         with pytest.raises(ValueError):
             utils._checksum(f, "chicken")
-        utils.sha256_checksum(f)
-        utils.md5_checksum(f)
+        assert utils.sha256_checksum(f) == "3b9c358f36f0a31b6ad3e14f309c7cf198ac9246e8316f9ce543d5b19ac02b80"
+        assert utils.md5_checksum(f) == "d41d8cd98f00b204e9800998ecf8427e"
