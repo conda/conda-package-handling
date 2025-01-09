8567b1633cb6a4cf70ca19692b9044a55b4a7810
Jannis Leidel <jannis@leidel.info>
2025-01-09 14:00:08 +0100

    Update tests/test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,7 +10,7 @@ def test_checksum(tmp_path):
         with pytest.raises(ValueError):
             utils._checksum(f, "chicken")
         assert (
-            utils.sha256_checksum(f)
-            == "3b9c358f36f0a31b6ad3e14f309c7cf198ac9246e8316f9ce543d5b19ac02b80"
+            utils.sha256_checksum(f) ==
+            "3b9c358f36f0a31b6ad3e14f309c7cf198ac9246e8316f9ce543d5b19ac02b80"
         )
         assert utils.md5_checksum(f) == "d41d8cd98f00b204e9800998ecf8427e"
