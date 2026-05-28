e0e8dae838fbb6188a6f49e12f2149fb4b662549
Ryan Keith <rkeith@anaconda.com>
2026-05-28 11:46:05 -0700

    Single pass file_list for pkg vs info.

--- src/conda_package_handling/conda_fmt.py
+++ src/conda_package_handling/conda_fmt.py
@@ -72,8 +72,9 @@ class CondaFormat_v2(AbstractBaseFormat):
             out_fn = os.path.basename(out_fn)
         conda_pkg_fn = os.path.join(out_folder, out_fn)
         file_id = out_fn[: -len(".conda")]
-        info_files = [f for f in file_list if utils.is_info_member_path(f)]
-        pkg_files = [f for f in file_list if not utils.is_info_member_path(f)]
+        pkg_files, info_files = [], []
+        for f in file_list:
+            (info_files if utils.is_info_member_path(f) else pkg_files).append(f)
 
         if compressor and (compression_tuple != (None, None, None)):
             raise ValueError("Supply one of compressor= or (deprecated) compression_tuple=")
